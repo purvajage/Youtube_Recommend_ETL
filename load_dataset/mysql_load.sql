78640b9e5b94e76ccd67b4a933963d29e914f777
purva jage <purvajage44@gmail.com>
2025-09-03 10:15:41 +0530

    commit

--- load_dataset/mysql_load.sql
+++ load_dataset/mysql_load.sql
@@ -10,6 +10,7 @@ FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n' 
 IGNORE 1 ROWS;
+
 LOAD DATA LOCAL INFILE '/tmp/youTube_trending_video/RU_youtube_trending_data.csv' 
 INTO TABLE RU_youtube_trending_data 
 FIELDS TERMINATED BY ',' 
